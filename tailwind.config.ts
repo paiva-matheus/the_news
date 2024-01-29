9c385120d89052bce9703c0b02383d2e874cd938
Matheus Paiva <matheuspaivavieirasilva@gmail.com>
2024-01-29 14:27:49 -0300

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
     },
+    colors: {
+      gray: '#97989F',
+      'blue-dark': '#181A2A',
+      'blue-light': '#4B6BFB',
+      'gray-dark': '#696A75',
+      'navy-blue': '#242535'
+    },
+    fontFamily: {
+      sans: ['Work Sans', 'sans-serif'],
+      serif: ['Source Serif 4', 'serif']
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
