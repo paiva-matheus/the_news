6f5e1c7fc7512378184d1232459f3cc5cf16c12d
Matheus Paiva <matheuspaivavieirasilva@gmail.com>
2024-02-05 14:08:46 -0300

    feat: add search input to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@/src/app/components/hero';
 import { TopHeadlines } from '@/src/app/components/top-headlines';
+import { HomeSearchInput } from './components/home-search-input';
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 dark:bg-gray-800">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 bg-slate-100 dark:bg-gray-800">
+      <HomeSearchInput />
       <Hero />
       <TopHeadlines />
     </main>
